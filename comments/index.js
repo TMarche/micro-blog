ca7cb077a3b13dc44370153940026492fa71376f
Troy Marchesseault <tmarchess@gmail.com>
2021-09-15 20:30:14 -0400

    Intermediary commit

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const {randomBytes} = require("crypto");
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -11,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id] || [])
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const {content} = req.body;
 
@@ -19,12 +22,49 @@ app.post("/posts/:id/comments", (req, res) => {
     const comments = commentsByPostId[req.params.id] || [];
     
     // Add the new comment to the list
-    comments.push({id: commentId, content});
+    comments.push({id: commentId, content, status: 'pending'});
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post("http://localhost:4005/events", {
+        type: "CommentCreated",
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id,
+            status: 'pending'
+        }
+    })
 
     res.status(201).send(comments);
 });
 
+app.post('/events', async (req, res) => {
+    console.log("Received Event:", req.body.type);
+
+    const {type, data} = req.body;
+
+    if (type === 'CommentModerated') {
+        const {postId, id, status, content} = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        })
+        comment.status = status;
+
+        await axios.post("http://localhost:4005/events", {
+            type: "CommentUpdated",
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        })
+    }
+
+    res.send({});
+});
+
 app.listen(4001, () => {
-    console.log("Listening on port 4001")
+    console.log("Comments listening on port 4001")
 })
