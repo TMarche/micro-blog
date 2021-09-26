4aad6e57ae4ad7ae5ef948e0860b298af759f54c
Troy Marchesseault <tmarchess@gmail.com>
2021-09-26 12:02:05 -0400

    Add posts Deployment and posts Service

--- posts/index.js
+++ posts/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
-const cors = require('cors');
-const axios = require('axios');
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,38 +10,39 @@ app.use(cors());
 
 const posts = {};
 
-
 // This is a test
 app.get("/posts", (req, res) => {
     res.send(posts);
 });
 
-
 // Body should have a title for the post
-app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const {title} = req.body;
+app.post("/posts", async (req, res) => {
+    const id = randomBytes(4).toString("hex");
+    const { title } = req.body;
 
     posts[id] = {
-        id, title
+        id,
+        title,
     };
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post("http://localhost:4005/events", {
         type: "PostCreated",
         data: {
-            id, title
-        }
+            id,
+            title,
+        },
     });
 
     res.status(201).send(posts[id]);
 });
 
-app.post('/events', (req, res) => {
+app.post("/events", (req, res) => {
     console.log("Received Event:", req.body.type);
 
     res.send({});
 });
 
 app.listen(4000, () => {
+    console.log("v55");
     console.log("Posts listening on 4000");
-})
+});
